0a2d2ffea0b4633c65d6667b5539c5993da21ed7
Paul S. <pavelslabunov@icloud.com>
2026-03-31 00:39:47 +0200

    Add default constructor and initialise member variables in BirthYear class

--- src/domain/BirthYear.h
+++ src/domain/BirthYear.h
@@ -8,6 +8,8 @@
 
 class BirthYear {
 public:
+    BirthYear() = default;
+
     explicit BirthYear(const uint16_t year, const bool approximate = false)
         : year_(year), is_approximate_(approximate) {
     }
@@ -16,6 +18,6 @@ public:
     [[nodiscard]] bool     is_approximate() const { return is_approximate_; }
 
 private:
-    uint16_t year_;
-    bool     is_approximate_;
+    uint16_t year_ = 0;
+    bool     is_approximate_ = false;
 };
